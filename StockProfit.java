da60aae51c08b5c4610213344504fcdaf4795799
yaminireddygaliveeti-creator <yaminireddygaliveeti@gmail.com>
2026-03-12 14:26:59 +0530

    Add files via upload

---
+++ StockProfit.java
@@ -0,0 +1,32 @@
+package pavani; 
+public class StockProfit {
+
+    public static int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            }
+
+            int profit = prices[i] - minPrice;
+
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+
+        int[] prices = {7, 1, 5, 3, 6, 4};
+
+        int profit = maxProfit(prices);
+
+        System.out.println("Maximum Profit: " + profit);
+    }
+}
